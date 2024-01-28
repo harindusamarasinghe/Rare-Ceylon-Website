14ec34a328ef016da8210e751c605732f7c31769
Shaun <shaun@Shauns-MacBook-Air.local>
2024-01-28 14:32:43 +0530

    Made the Home page responsive

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -10,7 +10,9 @@ const Home = () => {
                 <h1 className="home--heading">Digital Agency That Thrives on Your <span>Success</span></h1>
                 <p className="home--para">If you are looking for an agency to help you create a remarkable presence online, 
                     you’ve come to the right place. We can help you take your business to the next level.</p>
-                <button className="button-28 home">Contact us</button>
+                <div className="btn28">
+                  <button className="button-28 home">Contact us</button>
+                </div>
             </div>
 
         </div>
